fce45ea978605f849469353798f1992a145c8083
Rajendra4665 <75628700+Rajendra4665@users.noreply.github.com>
2021-01-11 18:57:11 +0530

    Add files via upload

--- game_auto/Voice_bot.py
+++ game_auto/Voice_bot.py
@@ -27,6 +27,7 @@ while bot_message != "Bye" or bot_message!='thanks':
     with sr.Microphone() as source:  # mention source it will be either Microphone or audio files.wwwwwwwwwwww
         print("Speak Anything :")
         audio = r.listen(source)  # listen to the source
+        
         try:
             message = r.recognize_google(audio)  # use recognizer to convert our audio into text part.
             print("You said : {}".format(message))
